8ebc75d4c39d35679c863411b9f0d7a3525d76bf
abejhall <abejhall@yahoo.com>
2017-08-17 11:12:16 -0400

    added the beginning of pathfinding
    this is just part of the pathfinding classes added

---
+++ PathFinding/Path_Edge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path_Edge<T>
+{
+    public float Cost;  //cost to traverse this edge cost to enter this class
+
+    public Path_Node<T> node;
+	
+}
---
+++ PathFinding/Path_Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path_Node<T>  {
+
+    public T data;  // Probably a tile but can be used for something else later
+
+    public Path_Edge<T>[] edges;  // Nodes leading OUT from this node
+
+    
+
+
+
+
+}
