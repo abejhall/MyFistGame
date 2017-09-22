ee2d3377e97ed370a09740390ce0bd7bf6aaf3ac
abejhall <abejhall@yahoo.com>
2017-09-22 02:45:38 -0400

    a temp solution for animation
     s for character till i get everthing drawn and on one sprite sheet

---
+++ WalkSpriteSwap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkSpriteSwap : MonoBehaviour {
+
+
+    public SpriteRenderer idle;
+    public SpriteRenderer walking;
+    public SpriteRenderer chopping;
+    CharacterStateMachine state;
+
+
+	// Use this for initialization
+	void Start () {
+        state = GetComponent<CharacterStateMachine>();
+        walking.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       if(state.CurrentState == CharacterStateMachine.StateMachine.MoveToJob || state.CurrentState == CharacterStateMachine.StateMachine.MoveToMaterial )
+        {
+            idle.enabled = false;
+            walking.enabled = true;
+            chopping.enabled = false;
+        }
+       else if(state.CurrentState == CharacterStateMachine.StateMachine.Working)
+        {
+            idle.enabled = false;
+            walking.enabled = false;
+            chopping.enabled = true;
+        }
+        else
+        {
+            idle.enabled = true;
+            walking.enabled = false;
+            chopping.enabled = false;
+
+        }
+		
+	}
+}
